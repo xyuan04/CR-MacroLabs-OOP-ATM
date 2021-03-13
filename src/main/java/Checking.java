217f5fcab79230e6c40bab7ab28ed68bbf1fa57f
hazel <hazel@zipcodes-MacBook-Pro-97.local>
2021-03-12 21:29:59 -0500

    Testing

---
+++ src/main/java/Checking.java
@@ -0,0 +1,22 @@
+public class Checking extends Account{
+
+    public Checking(Integer accountNumber, double balance) {
+        super(accountNumber,balance);
+    }
+
+    public void transferToSavings(Savings savingsAccount, double transferAmount) {
+        if (this.getBalance() > transferAmount && this.getBalance() - transferAmount >= 0) {
+            this.withdraw(transferAmount);
+            savingsAccount.deposit(transferAmount);
+        }
+
+        //return this.getBalance();
+    }
+
+    public void transferToInvestment(Investment investmentAccount, double transferAmount) {
+        if (this.getBalance() > transferAmount && this.getBalance() - transferAmount >= 0) {
+            this.withdraw(transferAmount);
+            investmentAccount.deposit(transferAmount);
+        }
+    }
+}
---
+++ src/main/java/Savings.java
@@ -0,0 +1,26 @@
+public class Savings {
+}
+public class Savings extends Account{
+
+    public Savings(Integer accountNumber, double balance) {
+        super(accountNumber, balance);
+    }
+
+    public double transferToChecking(Checking checkingAccount, double transferAmount) {
+        if (this.getBalance() > transferAmount && this.getBalance() - transferAmount >= 0) {
+            this.withdraw(transferAmount);
+            checkingAccount.deposit(transferAmount);
+        }
+
+        return this.getBalance();
+    }
+
+    public double transferToInvestment(Investment investmentAccount, double transferAmount) {
+        if (this.getBalance() > transferAmount && this.getBalance() - transferAmount >= 0) {
+            this.withdraw(transferAmount);
+            investmentAccount.deposit(transferAmount);
+        }
+
+        return this.getBalance();
+    }
+}
