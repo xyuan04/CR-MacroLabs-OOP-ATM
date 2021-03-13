76ab0f9cef17b3153dbc9b58df2be77d461a9b13
hazel <hazel@zipcodes-MacBook-Pro-97.local>
2021-03-13 13:54:31 -0500

    Checkpointing

--- src/main/java/Workflow.java
+++ src/main/java/Workflow.java
@@ -9,12 +9,15 @@ public class Workflow {
     }
 
     public void errorSP(){
-        d.currentDisplaySP("Improper input. Please reread instructions.");
+        d.currentDisplaySP("\nImproper input. Please reread instructions.\n");
     }
 
-    public Integer initialWelcomeSP(){
-        d.currentDisplaySP("Welcome to the ATM! Are you a new or returning user?");
-        return c.getIntegerInput("Enter 1 for new, 2 for returning:");
+    public void initialWelcomeSP(){
+        d.currentDisplaySP("Welcome to the ATM! Are you a new or returning user?\nEnter 1 for new, 2 for returning.");
+    }
+
+    public Integer getInput(){
+        return c.getIntegerInput("Enter your input:");
     }
 
 
@@ -35,14 +38,44 @@ public class Workflow {
         return c.getStringInput("Enter username:");
     }
 
-    public String getaPassSP(){
+    public String getPassSP(){
         return c.getStringInput("Enter password:");
     }
 
 
 
     public void mainMenuSP(Customer customer){
+        d.currentDisplaySP("Welcome, " + customer.getUserName() + "! What do you want to do?\nEnter 1 to check balance\t\t\t\tEnter 2 to transfer\nEnter 3 to withdraw\t\t\t\t\t\tEnter 4 to deposit\nEnter 5 to open a new account\t\t\tEnter 6 to close an account\nEnter 7 to print transaction history\tEnter 8 to log out");
+    }
 
+
+
+    public String enterAccount(){
+        return c.getStringInputNotCaseSensitive("Enter an account:");
+    }
+
+    public void checkPromptSP(){
+        d.currentDisplaySP("What account do you want to check?");
+    }
+
+    public void withdrawPromptSP(){
+        d.currentDisplaySP("What account do you want to withdraw from?");
+    }
+
+    public void depositPromptSP(){
+        d.currentDisplaySP("What account do you want to deposit into?");
+    }
+
+    public void transferPromptSP(){
+        d.currentDisplaySP("What accounts do you want to transfer from and to?");
+    }
+
+    public String openPrompt(){
+        return c.getStringInputNotCaseSensitive("What kind of account do you want to open?");
+    }
+
+    public void closePromptSP(){
+        d.currentDisplaySP("What account do you want to close?\nNOTE: The account must be empty.");
     }
 
 }
