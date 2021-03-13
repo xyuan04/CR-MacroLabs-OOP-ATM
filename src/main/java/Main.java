219c957f04774c95f93caff8d9c05ed3221f47c8
hazel <hazel@zipcodes-MacBook-Pro-97.local>
2021-03-13 17:18:30 -0500

    Filled out workflow

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,6 @@
 public class Main {
 
     public static void main(String[] args){
-        Workflow w = new Workflow();
-        Customer person = new Customer("Hazel", "12345", 67890);
-        w.mainMenuSP(person);
+        
     }
 }
