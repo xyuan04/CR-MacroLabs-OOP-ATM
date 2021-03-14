fc68c3a097f646277b86f7f55a902623941c2ecd
hazel <liam.k.becker@gmail.com>
2021-03-14 15:02:07 -0400

    Filled out check and history in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -115,6 +115,30 @@ public class Main {
 
     }
 
+
+
+    public void check(Workflow screen, Customer customer){
+        screen.checkPromptSP();
+
+        while(true) {
+            try {
+                Integer acctNumber = screen.enterAccount();
+                screen.checkResultSP(customer.getAccount(acctNumber));
+                break;
+            }
+            catch(Exception e){
+                screen.errorSP();
+            }
+        }
+    }
+
+    public void history(Workflow screen){
+        screen.printHistory();
+    }
+
+
+
+
     public void withdrawal(Workflow screen, Customer customer) {
         boolean validAmount = true;
         screen.withdrawPromptSP();
