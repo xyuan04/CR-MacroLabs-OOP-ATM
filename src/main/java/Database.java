1710bbe669a83b3d10f94a8395c1c414b77380ce
xiong <xiong@zipcodes-MBP-8.fios-router.home>
2021-03-14 14:58:56 -0400

    created and tested Database

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -1,7 +1,32 @@
 import java.util.ArrayList;
 
 public class Database {
-    private ArrayList<Customer> currentCustomers = new ArrayList<Customer>();
+    private static volatile ArrayList<Customer> currentCustomers = new ArrayList<Customer>();
+
+    public static void addCustomer(Customer customer) {
+        currentCustomers.add(customer);
+    }
+
+    public static Customer getCustomerByName(String name) {
+        for(Customer customer : currentCustomers) {
+            if(customer.getCustomerName().equals(name)) {
+                return customer;
+            }
+        }
+        return null;
+    }
+
+    public static void removeCustomer(Customer customer) {
+        currentCustomers.remove(customer);
+    }
+
+    public static Integer getNumberOfCustomers() {
+        return currentCustomers.size();
+    }
+
+    public static void removeAllCustomers() {
+        currentCustomers.clear();
+    }
 
 
 }
---
+++ src/main/test/java/DatabaseTest.java
@@ -0,0 +1,93 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DatabaseTest {
+
+    @Test
+    public void addCustomer() {
+        // : Given
+        Checking bruceChecking = Creator.createChecking(100);
+        Savings bruceSaving = Creator.createSavings(500);
+        Customer bruce = Creator.createCustomer("Bruce", "Bat", "Gotham", bruceChecking, bruceSaving);
+
+        // : When
+        Database.removeAllCustomers();
+        Database.addCustomer(bruce);
+        int expected = 1;
+        int actual = Database.getNumberOfCustomers();
+
+        System.out.println(Database.getNumberOfCustomers());
+
+        // : Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getCustomerByName() {
+        Checking bruceChecking = Creator.createChecking(100);
+        Savings bruceSaving = Creator.createSavings(500);
+        Checking batmanChecking = Creator.createChecking(500);
+        Savings batmanSavings = Creator.createSavings(1000);
+        Customer bruce = Creator.createCustomer("Bruce", "Bat", "Gotham", bruceChecking, bruceSaving);
+        Customer batman = Creator.createCustomer("Batman", "h", "y", batmanChecking, batmanSavings);
+
+        // : When
+        Database.removeAllCustomers();
+        Database.addCustomer(bruce);
+        Database.addCustomer(batman);
+        Customer expected = batman;
+        Customer actual = Database.getCustomerByName("Batman");
+
+        System.out.println(Database.getNumberOfCustomers());
+
+        // : Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeCustomer() {
+        Checking bruceChecking = Creator.createChecking(100);
+        Savings bruceSaving = Creator.createSavings(500);
+        Checking batmanChecking = Creator.createChecking(500);
+        Savings batmanSavings = Creator.createSavings(1000);
+        Customer bruce = Creator.createCustomer("Bruce", "Bat", "Gotham", bruceChecking, bruceSaving);
+        Customer batman = Creator.createCustomer("Batman", "h", "y", batmanChecking, batmanSavings);
+
+        // : When
+        Database.removeAllCustomers();
+        Database.addCustomer(bruce);
+        Database.addCustomer(batman);
+        Database.removeCustomer(bruce);
+        int expected = 1;
+        int actual = Database.getNumberOfCustomers();
+
+        System.out.println(Database.getNumberOfCustomers());
+
+        // : Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeAllCustomers() {
+        Checking bruceChecking = Creator.createChecking(100);
+        Savings bruceSaving = Creator.createSavings(500);
+        Checking batmanChecking = Creator.createChecking(500);
+        Savings batmanSavings = Creator.createSavings(1000);
+        Customer bruce = Creator.createCustomer("Bruce", "Bat", "Gotham", bruceChecking, bruceSaving);
+        Customer batman = Creator.createCustomer("Batman", "h", "y", batmanChecking, batmanSavings);
+
+        // : When
+        Database.addCustomer(bruce);
+        Database.addCustomer(batman);
+        Database.removeAllCustomers();
+        int expected = 0;
+        int actual = Database.getNumberOfCustomers();
+
+        System.out.println(Database.getNumberOfCustomers());
+
+        // : Then
+        Assert.assertEquals(expected, actual);
+    }
+}
